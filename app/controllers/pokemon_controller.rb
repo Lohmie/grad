0dbe27a2fd61fb688ece2b8dd7d2eb6d06069171
Mariam Habib <mariamjhabib@gmail.com>
2019-11-25 22:03:42 +0000

    cry

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -2,5 +2,9 @@ class PokemonController < ApplicationController
 
     def index
         @pokemon = PokeApi.get(pokemon: (1..807).to_a.sample).sprites.front_shiny
+
+        if @pokemon.nil?
+            @pokemon = "cry"
+        end
     end
 end
